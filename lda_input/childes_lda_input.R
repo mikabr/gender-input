35eca79dca095bf7ba5b128ab1f1a26cc0468809
Mika Braginsky <mikabr@stanford.edu>
2016-07-28 16:35:52 -0700

    lda input script

---
+++ lda_input/childes_lda_input.R
@@ -0,0 +1,53 @@
+library(dplyr)
+library(purrr)
+library(feather)
+library(SnowballC)
+
+childes_db <- do.call(src_mysql, yaml::yaml.load_file("db.yaml"))
+childes_tbl <- tbl(childes_db,
+                   sql("SELECT filename, speaker, gender, gloss FROM words"),
+                   n = -1)
+
+childes_words <- childes_tbl %>%
+  filter(speaker != "CHI") %>%
+  collect()
+
+childes_words_gender <- childes_words %>%
+  filter(gender != "")
+
+# mappify <- function(df, col, path) {
+#   value_list <- df[[col]] %>% unique() %>% sort()
+#   write(value_list, file = sprintf("%s/%s_map.txt", path, col))
+#   value_map <- setNames(1:length(value_list), value_list)
+#   value_index <- df[[col]] %>% map_int(~value_map[[.x]])
+#   write_feather(setNames(data_frame(index = value_index), col),
+#                 path = sprintf("%s/%s_index.feather", path, col))
+#   return()
+# }
+
+mappify <- function(df, col, path) {
+  value_list <- df[[col]] %>% unique() %>% sort()
+  write(value_list, file = sprintf("%s/%s_map.txt", path, col))
+  value_map <- data_frame(col = value_list, index = 1:length(value_list)) %>%
+    setNames(c(col, "index"))
+  value_index <- df %>% left_join(value_map) %>% select(index)
+  write_feather(setNames(value_index, col),
+                path = sprintf("%s/%s_index.feather", path, col))
+  return()
+}
+
+mappify(childes_words_gender, "gender", "data/unstemmed")
+mappify(childes_words_gender, "filename", "data/unstemmed")
+mappify(childes_words_gender, "gloss", "data/unstemmed")
+
+stem <- function(x) wordStem(x) %>% gsub("'", "", .)
+
+childes_stems <- childes_words_gender %>%
+  filter(!(substr(gloss, 1, 1) %in% c("&", "0", "")),
+         nchar(gloss) < 20) %>%
+  mutate(gloss = gsub("-.*", "", gloss),
+         gloss = ifelse(stem(gloss) == "", gloss, stem(gloss)))
+
+mappify(childes_stems, "gender", "data/stemmed")
+mappify(childes_stems, "filename", "data/stemmed")
+mappify(childes_stems, "gloss", "data/stemmed")
